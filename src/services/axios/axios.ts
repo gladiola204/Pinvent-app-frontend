c382cab46ec93a77c69439f36919be0f05486df8
Sylwia <stefansylwia@interia.pl>
2023-04-27 12:03:22 +0200

    update2 axios

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -4,7 +4,7 @@ const request = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
     validateStatus: () => true,
     withCredentials: true,
-    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }
+    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': '*' }
 })
 
 export default request;

5e4cf1fadf114389cbfcda45858658b1886e9f17
Sylwia <stefansylwia@interia.pl>
2023-04-27 15:16:53 +0200

    cookies

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -4,7 +4,7 @@ const request = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
     validateStatus: () => true,
     withCredentials: true,
-    headers: {'Access-Control-Allow-Origin': `${process.env.REACT_APP_BACKEND_URL}`, 'Content-Type': 'application/json'}
+    headers: {'Access-Control-Allow-Origin': `${process.env.REACT_APP_BACKEND_URL}`, 'Content-Type': 'application/json', "Access-Control-Allow-Credentials": true}
 })
 
 export default request;
